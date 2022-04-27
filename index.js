5e978dd8f72fbd73b44e59dc8bfe3e2c7f5c7d64
mhnnayem <mhnnayem@hotmail.com>
2022-04-28 03:14:40 +0800

    node for genius car

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors')
+var jwt = require('jsonwebtoken');
+
 require('dotenv').config()
 const port = process.env.Port || 5000;
 const app = express();
@@ -12,7 +14,25 @@ const res = require('express/lib/response');
 app.use(cors())
 app.use(express.json())
 
+function verifyJWtT(req, res, next) {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ massage: 'fobidden access' })
+        }
+        console.log('decoded', decoded)
+        req.decoded = decoded
+        next()
+    })
+    // console.log("inside verifyJWt", authHeader)
 
+
+
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bfdcb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -23,6 +43,16 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('genius-car-service').collection('service')
+        const orderCollection = client.db('genius-car-service').collection('order')
+
+        // auth
+        app.post('/login', async (req, res) => {
+            const user = req.body
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1d" })
+            res.send({ accessToken })
+        })
+
+        // services api
         app.get('/service', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query)
@@ -48,6 +78,29 @@ async function run() {
             const id = req.params.id
             const query = { _id: ObjectId(id) }
             const result = await serviceCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        // order collection api
+
+        app.get('/order', verifyJWtT, async (req, res) => {
+            const decodedEmail = req.decoded.email
+
+            const email = req.query.email
+            // console.log(email)
+            if (email === decodedEmail) {
+                const query = { email: email }
+                const cursor = orderCollection.find(query)
+                const orders = await cursor.toArray()
+                res.send(orders)
+            } else {
+                res.status(403).send({ massage: 'fobidden access' })
+            }
+        })
+
+        app.post('/order', async (req, res) => {
+            const order = req.body
+            const result = await orderCollection.insertOne(order)
             res.send(result)
         })
 
